99e8fb7e3348d153ffb3942ddd078d2ab63f47f6
cER <olgunburo09@gmail.com>
2025-11-15 21:54:35 +0300

    deleted reminder comment

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -39,6 +39,3 @@ g.loaded_node_provider = 0
 g.loaded_python3_provider = 0
 g.loaded_perl_provider = 0
 g.loaded_ruby_provider = 0
-
--- Misc ------------------------------------------------------------------------
--- (Add more groups later: splits, search, completion, etc.)
