b182313230d2596a82a7b58d6830599106dfce2a
cER <olgunburo09@gmail.com>
2026-01-02 00:12:30 +0300

    fix yamlls.lua

--- lua/lsp/yamlls.lua
+++ lua/lsp/yamlls.lua
@@ -1,13 +1,2 @@
-local schemas = require('schemastore').json.schemas()
-
 ---@type vim.lsp.Config
-local config = {
-  settings = {
-    json = {
-      schemas = schemas,
-      validate = { enable = true },
-    },
-  },
-}
-
-return config
+return {}
